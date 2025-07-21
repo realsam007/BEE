586de855b7601d509dbdc0e2cd4977a4dafa2fbd
Samarth Bhardwaj <samarthbhardwaj2@gmail.com>
2025-07-21 10:28:05 +0530

    Initial commit with Promise example

--- Lecture-7/index.js
+++ Lecture-7/index.js
@@ -16,3 +16,39 @@ p.then((data) => {
     console.log("Catch block");
     console.log(err);
 });
+
+
+const users = [
+    { id: 1, name: "Samarth", age: 19 },
+    { id: 2, name: "Yash", age: 17 },
+    { id: 3, name: "Anya", age: 22 }
+];
+
+function isAllowed(id) {
+    return new Promise((resolve, reject) => {
+        let user = null;
+
+        for (let i = 0; i < users.length; i++) {
+            console.log(users[i]);
+            if (users[i].id === id) {
+                user = users[i];
+                break;
+            }
+        }
+
+        if (!user) return reject("No user found");
+        if (user.age < 18) return reject("Age is less than 18");
+
+        resolve("Allowed");
+    });
+}
+
+// Call the function and handle the result
+isAllowed(1)
+    .then((data) => {
+        console.log("Success:", data);
+    })
+    .catch((err) => {
+        console.log("Error:", err);
+    });
+
